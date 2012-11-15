686ed61bdbe6d732971035fcdacc5e74722630be
et07cw6 <et07cw6@mars-2.student.lth.se>
2012-11-15 13:38:12 +0100

    kom igen nu

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import gui.GUI2;
+import gui.ImageHandler;
 import local.CameraListener;
 import local.Monitor;
 import server.PictureSender;
@@ -7,6 +9,7 @@ public class Main {
         Monitor monitor = new Monitor();
         (new PictureSender(6077)).start();
         (new CameraListener("localhost", 6077, 0, monitor)).start();
+        (new ImageHandler(monitor, (new GUI2()))).start();
 //        (new PictureSender(6078)).start();
 //        (new CameraListener("localhost", 6078, 1, monitor)).start();
     }

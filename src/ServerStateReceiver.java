97ee46200472b04bf1105fb58a07a47e7a239b78
et07cw6 <et07cw6@val-9.student.lth.se>
2012-11-21 11:43:17 +0100

    La till borttagna filer

---
+++ src/ServerStateReceiver.java
@@ -0,0 +1,33 @@
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+
+
+public class ServerStateReceiver extends Thread {
+	ServerMonitor serverMonitor;
+	String url;
+	int port;
+
+	public ServerStateReceiver(ServerMonitor serverMonitor, int port, String url) {
+		this.url = url;
+		this.port = port;
+		this.serverMonitor = serverMonitor;
+	}
+
+	public void run() {
+		try {
+			InputStream inputStream = (new Socket(url, port)).getInputStream();
+
+			while (true) {
+				int msg = inputStream.read();
+				serverMonitor.setMode(msg);
+			}
+		} catch (IOException e) {
+			System.out.println("Communication error with server: " + url + ":"
+					+ port + ".");
+			e.printStackTrace();
+		}
+	}
+}

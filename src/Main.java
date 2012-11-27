a2d22d5298841314d55cd9f7117ccbe2749ca5c6
Ludvig Nyqvist <ada10lny@lo-8.student.lth.se>
2012-11-27 15:43:04 +0100

    hej

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,8 @@ import se.lth.cs.fakecamera.Axis211A;
 public class Main {
 	public static void main(String[] args) {
         Axis211A camera = new Axis211A();
-        //(new CameraServer(6077, 6078, "localhost", 6079, camera)).start();
-        //(new CameraServer(6080, 6081, "localhost", 6082, camera)).start();
+        (new CameraServer(6077, 6078, "localhost", 6079, camera)).start();
+        (new CameraServer(6080, 6081, "localhost", 6082, camera)).start();
         new GUI();
 
 //        CameraClient cameraClient = new CameraClient(
